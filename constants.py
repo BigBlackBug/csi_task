b2d8fd1058a7a34314ca7c11196b4cc71a10b674
bigblackbug <bigblackbugg@gmail.com>
2017-06-20 01:14:53 +0300

    fixed a crash when classifiers folder does not exist

--- constants.py
+++ constants.py
@@ -2,6 +2,8 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 __CLASSIFIER_DIR = os.path.join(BASE_DIR, "classifiers")
+if not os.path.exists(__CLASSIFIER_DIR):
+    os.mkdir(__CLASSIFIER_DIR)
 
 
 def make_classifier_path(filename):
